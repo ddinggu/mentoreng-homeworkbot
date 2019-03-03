980743ee1f867586915a9840129416f207ea80b6
ddinggu <travelrudwls@gmail.com>
2019-03-03 12:55:43 +0900

    agenda wrong words

--- webhook/routes/homework.js
+++ webhook/routes/homework.js
@@ -11,7 +11,7 @@ export default ({ agenda }) => {
         console.log({ homework, time });
 
         // agenda.schedule :: run only once applied time
-        agenda.schdule(time, 'register homework', { homework });
+        agenda.schedule(time, 'register homework', { homework });
         ctx.body = {
             result: true,
             msg: '과제 등록 성공'
